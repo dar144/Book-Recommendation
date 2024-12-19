ecb5848923d48141c4133938c436156e996a5b27
Darya Haidukevich <90532518+dar144@users.noreply.github.com>
2024-12-19 13:57:20 +0100

    Update neo4j-driver.js

--- backend/neo4j-driver.js
+++ backend/neo4j-driver.js
@@ -1,8 +1,8 @@
 const neo4j = require("neo4j-driver");
 
 const driver = neo4j.driver(
-  "neo4j+s://f8dd7c21.databases.neo4j.io:7687", // Replace with your Neo4j instance URL
-  neo4j.auth.basic("neo4j", "rDrteUGdU5n5iEyfjHIOf0hvabkp3Y-HhW30fm_HFRM") // Replace with your username and password
+  "neo4j+s://f8dd7c21.databases.neo4j.io:7687", 
+  neo4j.auth.basic("neo4j", "rDrteUGdU5n5iEyfjHIOf0hvabkp3Y-HhW30fm_HFRM")
 );
 
 const session = driver.session();
